962a5ea83e386220136da2e902f6cbb41f4a3098
Adrian T. Saycon <adzbite@gmail.com>
2017-09-27 07:19:10 +0800

    fixes

--- View.php
+++ View.php
@@ -7,14 +7,22 @@ namespace arc\wp;
 */
 class View extends Core
 {
+	/**
+	 * View path
+	 * @var string
+	 */
 	public $viewPath = 'views';
 
-	public function render($filename)
+	/**
+	 * Responsible for rendering the wp admin menu
+	 * @param  string $filename the view name to be rendered
+	 */
+	public function renderAdmin($view)
 	{
 		if ( !current_user_can( 'manage_options' ) )  {
 			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 		}
-		$path = explode('/', $filename);
-		include APP_PATH . "/$this->viewPath/$filename.php";
+		echo "<div style='margin-top: 30px'></div>";
+		include APP_PATH . "/$this->viewPath/admin/$view.php";
 	}
 }
